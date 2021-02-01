0ab791d49b50523a7c839e2eb87fdc9714cb9b31
Felipe <felipe.pichl@hotmail.com>
2021-02-01 10:58:52 -0300

    NLW-01-Mobile-2.0 - End Home

--- nlw-01/version-2.0/mobile/src/pages/Home/index.tsx
+++ nlw-01/version-2.0/mobile/src/pages/Home/index.tsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Image, View, Text } from 'react-native';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Feather as Icon } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Content,
+  Description,
+  Main,
+  Title,
+  Input,
+  Button,
+  ButtonIcon,
+  ButtonText,
+} from './styles';
+
+import background from '../../assets/home-background.png';
+import logo from '../../assets/logo.png';
 
 const Home: React.FC = () => {
-  return <Container />;
+  const [uf, setUf] = useState('');
+  const [city, setCity] = useState('');
+
+  const { navigate } = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigate('Points', {
+      uf,
+      city,
+    });
+  }
+
+  return (
+    <Container>
+      <Content source={background} imageStyle={{ width: 274, height: 368 }}>
+        <Main>
+          <Image source={logo} />
+
+          <View>
+            <Title>Seu marketplace de coleta de resíduos</Title>
+            <Description>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
+            </Description>
+          </View>
+        </Main>
+        <View>
+          <Input
+            placeholder="Digite a UF"
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={setUf}
+          />
+
+          <Input
+            placeholder="Digite a cidade"
+            value={city}
+            autoCorrect={false}
+            onChangeText={setCity}
+          />
+
+          <Button onPress={handleNavigateToPoints}>
+            <ButtonIcon>
+              <Text>
+                <Icon name="arrow-right" color="#fff" size={24} />
+              </Text>
+            </ButtonIcon>
+
+            <ButtonText>Entrar</ButtonText>
+          </Button>
+        </View>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
--- nlw-01/version-2.0/mobile/src/pages/Home/styles.ts
+++ nlw-01/version-2.0/mobile/src/pages/Home/styles.ts
@@ -1,6 +1,12 @@
+import { ImageBackgroundBase, KeyboardAvoidingView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(KeyboardAvoidingView)`
+  flex: 1;
+`;
+
+export const Content = styled(ImageBackgroundBase)`
   flex: 1;
   padding: 32px;
 `;
@@ -16,4 +22,50 @@ export const Title = styled.Text`
   font-family: 'Ubuntu_700Bold';
   max-width: 260px;
   margin-top: 64px;
+`;
+
+export const Description = styled.Text`
+  color: '#6C6C80';
+  font-size: 16px;
+  margin-top: 16px;
+  font-family: 'Roboto_400Regular';
+  max-width: 260px;
+  line-height: 24px;
+`;
+
+export const Input = styled.TextInput`
+  background-color: '#34CB79';
+  height: 60;
+  flex-direction: 'row';
+  border-radius: 10;
+  overflow: 'hidden';
+  align-items: 'center';
+  margin-top: 8;
+`;
+
+export const Button = styled(RectButton)`
+  background-color: '#34CB79';
+  height: 60px;
+  flex-direction: 'row';
+  border-radius: 10px;
+  overflow: 'hidden';
+  align-items: 'center';
+  margin-top: 8px;
+`;
+
+export const ButtonIcon = styled.View`
+  height: 60px;
+  width: 60px;
+  background-color: 'rgba(0, 0, 0, 0.1)';
+  justify-content: 'center';
+  align-items: 'center';
+`;
+
+export const ButtonText = styled.Text`
+  flex: 1;
+  justify-content: 'center';
+  text-align: 'center';
+  color: '#FFF';
+  font-family: 'Roboto_500Medium';
+  font-size: 16px;
 `;
