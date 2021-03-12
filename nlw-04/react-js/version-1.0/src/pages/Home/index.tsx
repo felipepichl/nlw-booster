cc8fa3696ffd8670dfe7721e28469349f8b4abde
Felipe <felipe.pichl@hotmail.com>
2021-03-12 09:54:08 -0300

    NLW-04 - React-JS end day 01

--- nlw-04/react-js/version-1.0/src/pages/Home/index.tsx
+++ nlw-04/react-js/version-1.0/src/pages/Home/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import ExperienceBar from '../../components/ExperienceBar';
+
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Hello World - Home</h1>
+      <ExperienceBar />
     </Container>
   );
 };
