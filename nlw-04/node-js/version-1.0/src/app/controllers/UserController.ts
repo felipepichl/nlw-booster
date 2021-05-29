e2f947c3ef2038365281c0923e05dc9298becbe0
Felipe Pichl <felipe.pichl@hotmail.com>
2021-05-28 22:17:24 -0300

    NLW-04 - Node-JS: Done day 2

--- nlw-04/node-js/version-1.0/src/app/controllers/UserController.ts
+++ nlw-04/node-js/version-1.0/src/app/controllers/UserController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
 
 class UserController {
   public async store(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
 
-    return response.json({ name, email });
+    const userRepository = getRepository(User);
+
+    const userExists = userRepository.findOne({ where: email });
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+    });
+
+    await userRepository.save(user);
+
+    return response.json(user);
   }
 }
 
