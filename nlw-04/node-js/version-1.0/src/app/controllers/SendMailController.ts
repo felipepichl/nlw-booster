0f003e900d6f7a64dd8b7f5aaebd81349c797e82
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-04 20:45:57 -0300

    NLW-04 - Node-JS: Doing SendMailController

--- nlw-04/node-js/version-1.0/src/app/controllers/SendMailController.ts
+++ nlw-04/node-js/version-1.0/src/app/controllers/SendMailController.ts
@@ -1,3 +1,20 @@
-class SendMailController {}
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import SurveysRepository from 'app/repositories/SurveysRepository';
+import SurveysUsersRepository from 'app/repositories/SurveysUsersRepository';
+import UsersRepository from 'app/repositories/UsersRepository';
+
+class SendMailController {
+  public async execute(request: Request, response: Response): Promise<void> {
+    const { email, survey_id } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+    const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const userAlreadyExists = usersRepository.findOne(email);
+  }
+}
 
 export default SendMailController;
