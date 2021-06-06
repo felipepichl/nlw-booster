b1a05ffa246472fcc1660f9969fba3e24f4dcd8e
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-06 16:40:16 -0300

    NLW-04 - Node-JS: Done SendMail without email

--- nlw-04/node-js/version-1.0/src/app/controllers/SendMailController.ts
+++ nlw-04/node-js/version-1.0/src/app/controllers/SendMailController.ts
@@ -6,14 +6,42 @@ import SurveysUsersRepository from 'app/repositories/SurveysUsersRepository';
 import UsersRepository from 'app/repositories/UsersRepository';
 
 class SendMailController {
-  public async execute(request: Request, response: Response): Promise<void> {
+  public async execute(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
     const { email, survey_id } = request.body;
 
     const usersRepository = getCustomRepository(UsersRepository);
     const surveysRepository = getCustomRepository(SurveysRepository);
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-    const userAlreadyExists = usersRepository.findOne(email);
+    const userAlreadyExists = usersRepository.findOne({ email });
+
+    if (!userAlreadyExists) {
+      return response.status(400).json({
+        error: 'User does not exists',
+      });
+    }
+
+    const surveyAlreadyExists = await surveysRepository.findOne({
+      id: survey_id,
+    });
+
+    if (!surveyAlreadyExists) {
+      return response.status(400).json({
+        error: 'Survey does not exists',
+      });
+    }
+
+    const surveyUser = surveysUsersRepository.create({
+      survey_id,
+      user_id: (await userAlreadyExists).id,
+    });
+
+    await surveysUsersRepository.save(surveyUser);
+
+    return response.json(surveyUser);
   }
 }
 
--- nlw-04/node-js/version-1.0/src/routes/index.ts
+++ nlw-04/node-js/version-1.0/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import SendMailController from '@controller/SendMailController';
 import UserController from '../app/controllers/UsersController';
 import SurveysController from '../app/controllers/SurveysController';
 
@@ -7,10 +8,13 @@ const router = Router();
 
 const usersController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post('/users', usersController.store);
 
 router.post('/surveys', surveysController.store);
 router.get('/surveys', surveysController.show);
+
+router.post('/sendMail', sendMailController.execute);
 
 export default router;
