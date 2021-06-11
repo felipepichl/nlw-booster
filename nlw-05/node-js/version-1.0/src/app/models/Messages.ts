2d3377048a979b62096bb3c92d36416f9e15f034
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-11 20:08:34 -0300

    NLW-05 - Node-JS: Messages migration and entitie

---
+++ nlw-05/node-js/version-1.0/src/app/models/Messages.ts
@@ -0,0 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+import { User } from './User';
+
+@Entity('messages')
+class Messages {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  text: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Messages };
