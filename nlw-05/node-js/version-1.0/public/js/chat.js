a1fce240c383cdfb87775563b17265d23ceb1c2f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-22 23:52:02 -0300

    NLW-05 - Node-JS: List all messages

--- nlw-05/node-js/version-1.0/public/js/chat.js
+++ nlw-05/node-js/version-1.0/public/js/chat.js
@@ -26,7 +26,27 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
   });
 
   socket.on('client_list_all_messages', messages => {
-    console.log('messages => ', messages)
+    var template_client = document.getElementById('message-user-template').innerHTML;
+    var template_admin = document.getElementById('admin-template').innerHTML;
+
+    messages.forEach(message => {
+      if(message.admin_id === null) {
+        const render = Mustache.render(template_client, {
+          message: message.text,
+          email,
+        });
+
+        document.getElementById('messages').innerHTML += render;
+      } else {
+        const render = Mustache.render(template_admin, {
+          message_admin: message.text,
+          email,
+        });
+
+        document.getElementById('messages').innerHTML += render;
+      }
+    });
+
   })
 });
 
