5ba6ba485d1067f26f8167052989f03847b1a7ad
Felipe Pichl <felipe.pichl@hotmail.com>
2021-06-26 14:30:01 -0300

    NLW-05 - Node-JS: Show support messages

--- nlw-05/node-js/version-1.0/public/js/admin.js
+++ nlw-05/node-js/version-1.0/public/js/admin.js
@@ -48,18 +48,18 @@ function call(id){
       if(message.admin_id === null){
         createDiv.className = 'admin_message_client';
 
-        createDiv.innerHTML = `<span>${connection.user.email}</span>`
-        createDiv.innerHTML += `<span>${message.text}</span>`
+        createDiv.innerHTML = `<span>${connection.user.email}</span>`;
+        createDiv.innerHTML += `<span>${message.text}</span>`;
 
         createDiv.innerHTML += `<span class="admin_date">
-          ${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}</span>`
+          ${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}</span>`;
       } else {
         createDiv.className = 'admin_message_admin';
 
-        createDiv.innerHTML = `Atendente: <span>${message.text}</span>`
+        createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
 
         createDiv.innerHTML += `<span class="admin_date">
-          ${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}</span>`
+          ${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}</span>`;
       }
 
       divMessages.appendChild(createDiv);
@@ -78,4 +78,17 @@ function sendMessage(id) {
   };
 
   socket.emit('admin_send_message', params);
+
+  const divMessages = document.getElementById(`allMessages${id}`);
+  const createDiv = document.createElement('div');
+
+  createDiv.className = 'admin_message_admin';
+  createDiv.innerHTML = `Atendente: <span>${params.text}</span>`;
+
+  createDiv.innerHTML += `<span class="admin_date">${dayjs()
+    .format('DD/MM/YYYY HH:mm:ss')}</span>`;
+
+  divMessages.appendChild(createDiv);
+
+  text.value = '';
 }
