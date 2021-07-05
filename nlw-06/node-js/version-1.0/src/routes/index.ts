6fa8a0d0c529aad68f3f50e277a3f1262e1152cf
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-04 23:35:46 -0300

    NLW-06 - Node-JS: Middleware Admin

--- nlw-06/node-js/version-1.0/src/routes/index.ts
+++ nlw-06/node-js/version-1.0/src/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { CreateUsersControllers } from '@controller/CreateUsersControllers';
 import { CreateTagsController } from '@controller/CreateTagsController';
 
+import { ensureAdmin } from '../app/middlewares/ensureAdmin';
+
 const router = Router();
 
 const createUsersControllers = new CreateUsersControllers();
 const createTagsController = new CreateTagsController();
 
 router.post('/users', createUsersControllers.handle);
-router.post('/tags', createTagsController.handle);
+router.post('/tags', ensureAdmin, createTagsController.handle);
 
 export default router;
