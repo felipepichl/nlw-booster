ededdc39bf4367230b74240b88b4a510e95ff0c3
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-09 22:20:52 -0300

    NLW-06 - Node-JS: Compliments entity

---
+++ nlw-06/node-js/version-1.0/src/app/models/Compliment.ts
@@ -0,0 +1,58 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+import { User } from '@models/User';
+import { Tag } from '@models/Tag';
+
+@Entity('compliments')
+class Compliment {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_sender: string;
+
+  @JoinColumn({ name: 'user_sender' })
+  @ManyToOne(() => User)
+  userSender: User;
+
+  @Column()
+  user_receiver: string;
+
+  @JoinColumn({ name: 'user_receiver' })
+  @ManyToOne(() => User)
+  userReceiver: User;
+
+  @Column()
+  tag_id: string;
+
+  @JoinColumn({ name: 'tag_id' })
+  @ManyToOne(() => Tag)
+  tag: Tag;
+
+  @Column()
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Compliment };
