729d634a9a57a6a2e39f47a4a18dcedc1ad8b088
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-13 20:55:58 -0300

    NLW-06 - Node-JS: List sender/receive service

---
+++ nlw-06/node-js/version-1.0/src/app/services/ListUserSenderComplimentsService.ts
@@ -0,0 +1,26 @@
+import { Repository, getCustomRepository } from 'typeorm';
+
+import { Compliment } from '@models/Compliment';
+import { ComplimentsRepositories } from '../repositories/ComplimentsRepositories';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListUserSenderComplimentsService {
+  private complimentsRepository: Repository<Compliment>;
+
+  constructor() {
+    this.complimentsRepository = getCustomRepository(ComplimentsRepositories);
+  }
+
+  public async execute({ user_id }: IRequest): Promise<Compliment[]> {
+    const compliments = await this.complimentsRepository.find({
+      where: { user_sender: user_id },
+    });
+
+    return compliments;
+  }
+}
+
+export { ListUserSenderComplimentsService };
