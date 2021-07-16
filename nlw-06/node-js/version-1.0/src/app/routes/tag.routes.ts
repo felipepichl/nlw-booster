7680b6147d5f1401c8f7026449e3a50bb78322ad
Felipe <felipe.pichl@hotmail.com>
2021-07-16 10:36:41 -0300

    NLW-06 - Node-JS: List tags routes

--- nlw-06/node-js/version-1.0/src/app/routes/tag.routes.ts
+++ nlw-06/node-js/version-1.0/src/app/routes/tag.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 
 import { CreateTagsController } from '@controller/CreateTagsController';
+import { ListTagController } from '@controller/ListTagController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const createTagRouter = Router();
 
 const createTagsController = new CreateTagsController();
+const listTagsController = new ListTagController();
 
-createTagRouter.post('/', ensureAuthenticated, createTagsController.handle);
+createTagRouter.use(ensureAuthenticated);
+
+createTagRouter.post('/', createTagsController.handle);
+createTagRouter.get('/', listTagsController.handle);
 
 export { createTagRouter };
