e56fcb7fcadc2c3e41184cb7ff8f1b5bf6c0a611
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-25 22:33:23 -0300

    NLW-02-Server-2.0 - Subjects services

---
+++ nlw-02/version-2.0/server/src/app/services/Subjects/CreateSubjectsServices.ts
@@ -0,0 +1,36 @@
+import { Repository, getCustomRepository } from 'typeorm';
+
+import { AppError } from 'app/error/AppError';
+
+import { Subject } from '@entities/Subject';
+import { SubjectsRepository } from '../../repositories/SubjectsRepository';
+
+interface IRequest {
+  title: string;
+}
+
+class CreateSubjectsServices {
+  private subjectRepository: Repository<Subject>;
+
+  constructor() {
+    this.subjectRepository = getCustomRepository(SubjectsRepository);
+  }
+
+  public async execute({ title }: IRequest): Promise<Subject> {
+    const subjectAllReadyExists = await this.subjectRepository.find({ title });
+
+    if (subjectAllReadyExists) {
+      throw new AppError('Subject already exists');
+    }
+
+    const subject = this.subjectRepository.create({
+      title,
+    });
+
+    await this.subjectRepository.save(subject);
+
+    return subject;
+  }
+}
+
+export { CreateSubjectsServices };
