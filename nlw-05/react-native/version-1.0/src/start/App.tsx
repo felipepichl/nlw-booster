f5ddfd0fdf2262e9848b478b2cfd1d95a29306b4
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-27 21:57:01 -0300

    NLW-05-Mobile-1.0 - Done day 1

--- nlw-05/react-native/version-1.0/src/start/App.tsx
+++ nlw-05/react-native/version-1.0/src/start/App.tsx
@@ -1,23 +1,12 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+
+import { Welcome } from '../pages/Welcome';
 
 const App: React.FC = () => {
-  return (
-    <View style={style.container}>
-      <Text>Hello my friend</Text>
-    </View>
-  );
+  return <Welcome />;
 };
 
 registerRootComponent(App);
 
 export default App;
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
