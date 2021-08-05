7c894273ebf3fcc46611c6e7b2cc5b9ce8140eea
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-05 18:56:03 -0300

    NLW-02-Server-2.0 - Username field, migration

---
+++ nlw-02/version-2.0/server/src/database/migrations/1628200389067-AddUsernameFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddUsernameFieldToUsers1628200389067
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'username',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'username');
+  }
+}
