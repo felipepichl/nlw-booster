8f468c34f1395c7181580692d964df9d53356ecd
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-09 18:09:02 -0300

    NLW-02-Server-2.0 - Refactor ClassSchadule

--- nlw-02/version-2.0/server/src/app/controllers/ClassesSchedule/CreateClassScheduleController.ts
+++ nlw-02/version-2.0/server/src/app/controllers/ClassesSchedule/CreateClassScheduleController.ts
@@ -2,34 +2,18 @@ import { Request, Response } from 'express';
 
 import { CreateClassScheduleServices } from 'app/services/ClassSchedule/CreateClassScheduleServices';
 
-import convertHourToMinutes from '../../utils/convertHourToMinutes';
-
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
-
 class CreateClassScheduleController {
   public async handle(request: Request, response: Response): Promise<Response> {
     const { class_id, schedule } = request.body;
 
     const createClassScheduleServices = new CreateClassScheduleServices();
 
-    const createClassSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-        class_id,
-      };
+    createClassScheduleServices.execute({
+      class_id,
+      scheduleItem: schedule,
     });
 
-    const classSchedule = await createClassScheduleServices.execute(
-      createClassSchedule,
-    );
-
-    return response.json(classSchedule);
+    return response.json().status(201);
   }
 }
 
--- nlw-02/version-2.0/server/src/app/services/ClassSchedule/CreateClassScheduleServices.ts
+++ nlw-02/version-2.0/server/src/app/services/ClassSchedule/CreateClassScheduleServices.ts
@@ -3,10 +3,14 @@ import { Repository, getCustomRepository } from 'typeorm';
 import { ClassSchedule } from '@entities/ClassSchedule';
 import { ClassScheduleRepository } from 'app/repositories/ClassScheduleRepository';
 
+import convertHourToMinutes from '../../utils/convertHourToMinutes';
+
 interface IRequest {
-  week_day: number;
-  from: number;
-  to: number;
+  scheduleItem: {
+    week_day: number;
+    from: number;
+    to: number;
+  }[];
   class_id: string;
 }
 
@@ -17,22 +21,19 @@ class CreateClassScheduleServices {
     this.classScheduleRepository = getCustomRepository(ClassScheduleRepository);
   }
 
-  public async execute({
-    week_day,
-    from,
-    to,
-    class_id,
-  }: IRequest): Promise<ClassSchedule> {
-    const classSchedule = this.classScheduleRepository.create({
-      week_day,
-      from,
-      to,
-      class_id,
+  public async execute({ scheduleItem, class_id }: IRequest): Promise<void> {
+    scheduleItem.map(async schedule => {
+      const createClassSchedule = this.classScheduleRepository.create({
+        week_day: schedule.week_day,
+        from: convertHourToMinutes(String(schedule.from)),
+        to: convertHourToMinutes(String(schedule.to)),
+        class_id,
+      });
+
+      await this.classScheduleRepository.save(createClassSchedule);
+
+      return createClassSchedule;
     });
-
-    await this.classScheduleRepository.save(classSchedule);
-
-    return classSchedule;
   }
 }
 
