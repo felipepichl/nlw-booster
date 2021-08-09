e01fcef415d9617c45d47bf0fa39864e61a6793e
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-09 14:05:46 -0300

    NLW-02-Server-2.0 - Fix Class ensureAuthenticated

--- nlw-02/version-2.0/server/src/app/routes/classes.routes.ts
+++ nlw-02/version-2.0/server/src/app/routes/classes.routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express';
 
 import { CreateClassesController } from '../controllers/Classes/CreateClassesController';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const createClassesRouter = Router();
 
 const createClassesController = new CreateClassesController();
+
+createClassesRouter.use(ensureAuthenticated);
 
 createClassesRouter.post('/', createClassesController.handle);
 
