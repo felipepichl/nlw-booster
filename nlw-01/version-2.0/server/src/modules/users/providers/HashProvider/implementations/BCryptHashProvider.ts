3435a05dbcbc9efb3e9cb4d341cce6d49b69c89b
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-14 21:18:00 -0300

    NLW-01-Server-2.0 - Fix implementations

--- nlw-01/version-2.0/server/src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ nlw-01/version-2.0/server/src/modules/users/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -2,11 +2,11 @@ import { hash, compare } from 'bcryptjs';
 import { IHashProvider } from '../models/IHashProvider';
 
 class BCryptHashProvider implements IHashProvider {
-  public generateHash(payload: string): Promise<string> {
+  public async generateHash(payload: string): Promise<string> {
     return hash(payload, 8);
   }
 
-  public compareHash(payload: string, hashed: string): Promise<boolean> {
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
     return compare(payload, hashed);
   }
 }
