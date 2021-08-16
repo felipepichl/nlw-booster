4cf7356e25070b8f4d10d34f9a78c74c68af7b19
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-16 14:13:55 -0300

    NLW-01-Server-2.0 - Fake users repository

---
+++ nlw-01/version-2.0/server/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid';
+
+import { User } from '@modules/users/infra/typeorm/entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+
+class FakeUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+}
+
+export { FakeUsersRepository };
