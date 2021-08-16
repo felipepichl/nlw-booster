d8141aaadffb1bf5f0120774e50b6cde7269a758
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-16 20:45:16 -0300

    NLW-05-Mobile-1.0 - Done Welcome page

--- nlw-05/react-native/version-1.0/src/start/App.tsx
+++ nlw-05/react-native/version-1.0/src/start/App.tsx
@@ -1,9 +1,24 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
 
 import { Welcome } from '../pages/Welcome';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Welcome />;
 };
 
