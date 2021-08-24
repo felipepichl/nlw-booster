a542306b3513cf78ade7f254d778ba898eced725
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-24 20:58:17 -0300

    NLW-01-Server-2.0 - Users controller

---
+++ nlw-01/version-2.0/server/src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserServices } from '../../../services/CreateUserServices';
+
+class UsersController {
+  public async execute(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const { username, name, email, password } = request.body;
+
+    const usersService = container.resolve(CreateUserServices);
+
+    const user = await usersService.execute({
+      username,
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
+
+export { UsersController };
