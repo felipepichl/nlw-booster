71f2637dc9f925c61bf2784857293706e08aecad
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-23 21:37:49 -0300

    NLW-05-Mobile-1.0 - Done Header CSS

--- nlw-05/react-native/version-1.0/src/components/Header.tsx
+++ nlw-05/react-native/version-1.0/src/components/Header.tsx
@@ -3,13 +3,20 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
+import userImg from '../assets/rodrigo.png';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 const Header: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Olá</Text>
-      {/* <Image /> */}
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Felipe Pichl</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image} />
     </View>
   );
 };
@@ -22,8 +29,26 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: '20',
     marginTop: getStatusBarHeight(),
-    backgroundColor: colors.red,
+    padding: 20,
+  },
+
+  greeting: {
+    fontSize: 32,
+    fontFamily: fonts.text,
+    color: colors.heading,
+  },
+
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
   },
 });
