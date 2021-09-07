244fdb7f0124aa2925b4c24cad0b73b1bbb09ace
Felipe Pichl <felipe.pichl@hotmail.com>
2021-09-07 19:54:55 -0300

    NLW-01-Server-2.0 - Done CreateSessionServices

--- nlw-01/version-2.0/server/src/modules/users/services/CreateSessionServices.ts
+++ nlw-01/version-2.0/server/src/modules/users/services/CreateSessionServices.ts
@@ -1,7 +1,13 @@
 import { injectable, inject } from 'tsyringe';
 
+import authConfig from '@config/authConfig';
+
+import { AppError } from '@shared/errors/AppError';
+
+import { sign } from 'jsonwebtoken';
 import { User } from '../infra/typeorm/entities/User';
 import { IUsersRepository } from '../repositories/IUsersRepository';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 
 interface IResponse {
   user: User;
@@ -18,10 +24,35 @@ class CreateSessionServices {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
-    // const user = this.usersRepository.
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect Email/Password');
+    }
+
+    const passwordMatch = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatch) {
+      throw new AppError('Incorrect Email/Password');
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
   }
 }
 
