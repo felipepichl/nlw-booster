cf63e94a6b11969538c85e83d67270b04b358bb6
Felipe Pichl <felipe.pichl@hotmail.com>
2021-09-09 21:50:25 -0300

    NLW-01-Server-2.0 - Done session routes

--- nlw-01/version-2.0/server/src/shared/infra/http/routes/index.ts
+++ nlw-01/version-2.0/server/src/shared/infra/http/routes/index.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import { itemsRouter } from '@modules/items/infra/http/routes/items.routes';
 import { pointRouter } from '@modules/Points/infra/http/routes/points.routes';
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes';
+import { sessionRouter } from '@modules/users/infra/http/routes/session.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/session', sessionRouter);
 
 routes.use('/items', itemsRouter);
 routes.use('/points', pointRouter);
