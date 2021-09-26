63435248fbbd85d9a389f0c74174f08fbad782d8
Felipe Pichl <felipe.pichl@hotmail.com>
2021-09-26 20:27:25 -0300

    NLW-06-ReactNative: Day-2: Doing Avatar

--- nlw-06/react-native/version-1.0/src/components/Profile/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Profile/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { styles } from './styles';
 
 const Profile: React.FC = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá</Text>
+
+          <Text>Felipe Pichl</Text>
+        </View>
+        <Text style={styles.message}>Today, Is day for win</Text>
+      </View>
+    </View>
+  );
 };
 
 export { Profile };
