ff88d29e366f103f24e3c83a59c088ef2f54819b
Felipe Pichl <felipe.pichl@hotmail.com>
2021-09-26 20:42:35 -0300

    NLW-06-ReactNative: Day-2: Done Avatar

--- nlw-06/react-native/version-1.0/src/components/Avatar/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Avatar/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
-const Avatar: React.FC = () => {
-  return <View />;
+interface AvatarProps {
+  urlImage: string;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ urlImage }) => {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary50, secondary70]}
+    >
+      <Image source={{ uri: urlImage }} style={styles.avatar} />
+    </LinearGradient>
+  );
 };
 
-export default Avatar;
+export { Avatar };
--- nlw-06/react-native/version-1.0/src/components/Profile/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Profile/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { Avatar } from '../Avatar';
+
 import { styles } from './styles';
 
 const Profile: React.FC = () => {
   return (
     <View style={styles.container}>
+      <Avatar urlImage="http://github.com/felipepichl.png" />
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
