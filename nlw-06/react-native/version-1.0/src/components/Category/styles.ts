a04f46c9c71767afe4c92b733af59419c95dff68
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-01 22:46:20 -0300

    NLW-06-ReactNative: Day-2: Category css

--- nlw-06/react-native/version-1.0/src/components/Category/styles.ts
+++ nlw-06/react-native/version-1.0/src/components/Category/styles.ts
@@ -1,10 +1,41 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 const styles = StyleSheet.create({
-  container: {},
-  checked: {},
-  check: {},
-  title: {},
+  container: {
+    width: 104,
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    marginRight: 8,
+  },
+  checked: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
+    width: 10,
+    height: 10,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 3,
+  },
+  check: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
+    width: 12,
+    height: 12,
+    backgroundColor: theme.colors.secondary100,
+    borderColor: theme.colors.secondary50,
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+  title: {
+    fontFamily: theme.fonts.title700,
+    fontSize: 15,
+    color: theme.colors.heading,
+    marginTop: 15,
+  },
 });
 
 export { styles };
