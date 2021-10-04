ece030c9e4ed2ea2eacb06a7170659a57be2c58c
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-04 11:59:44 -0300

    NLW-06-ReactNative: Day-2: Doing Appointment css

--- nlw-06/react-native/version-1.0/src/components/Appointment/styles.ts
+++ nlw-06/react-native/version-1.0/src/components/Appointment/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  content: {
+    flex: 1,
+  },
 });
 
 export { styles };
