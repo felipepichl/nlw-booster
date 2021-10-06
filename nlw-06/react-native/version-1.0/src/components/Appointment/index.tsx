8a6597a91cc5b96691129f3c025403302a105c51
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-05 21:17:41 -0300

    NLW-06-ReactNative: Day-2: Doing GuildIcon

--- nlw-06/react-native/version-1.0/src/components/Appointment/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Appointment/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { RectButton } from 'react-native-gesture-handler';
+
 import { View } from 'react-native';
 
 import { styles } from './styles';
+import { GuildIcon } from '../GuildIcon';
 
-const Appointment: React.FC = () => {
-  return <View style={styles.container} />;
+interface GuildProps {
+  id: string;
+  name: string;
+  icon: string;
+  owner: boolean;
+}
+
+interface AppointmentProps {
+  id: string;
+  guild: GuildProps;
+  category: string;
+  date: string;
+  description: string;
+}
+
+interface Props extends RectButton {
+  data: AppointmentProps;
+}
+
+const Appointment: React.FC<Props> = ({ data, ...rest }) => {
+  return (
+    <RectButton style={styles.container} {...rest}>
+      <View style={styles.content}>
+        <GuildIcon />
+      </View>
+    </RectButton>
+  );
 };
 
-export { Appointment };
+export { Appointment, GuildProps, AppointmentProps };
