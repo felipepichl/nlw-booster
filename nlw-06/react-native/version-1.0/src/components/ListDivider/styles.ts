b72bc853219fbc8318bf766d7ed85dcf1324d82a
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-08 22:19:43 -0300

    NLW-06-ReactNative: Day-2: Done ListDivider

--- nlw-06/react-native/version-1.0/src/components/ListDivider/styles.ts
+++ nlw-06/react-native/version-1.0/src/components/ListDivider/styles.ts
@@ -1,7 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    height: 1,
+    width: '78%',
+    alignSelf: 'flex-end',
+    backgroundColor: theme.colors.secondary40,
+  },
 });
 
 export { styles };
