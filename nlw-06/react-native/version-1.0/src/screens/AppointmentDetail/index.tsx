9aa21de98ccdabde62e25c10446e9439e5f1bfd0
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-12 20:03:03 -0300

    NLW-06-ReactNative: Day-3: Doing Header

--- nlw-06/react-native/version-1.0/src/screens/AppointmentDetail/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentDetail/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { Background } from '../../components/Background';
+
 import { styles } from './styles';
 
 const AppointmentDetail: React.FC = () => {
-  return <View style={styles.container} />;
+  return <Background />;
 };
 
 export default AppointmentDetail;
