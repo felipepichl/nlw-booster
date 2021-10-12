712aafd87fe14a644b5d23da7b2aae400d2d607e
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-12 20:25:40 -0300

    NLW-06-ReactNative: Day-3: Done Header

--- nlw-06/react-native/version-1.0/src/components/Header/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Header/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { ReactNode } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
+import { Text, View } from 'react-native';
+
+import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
-const Header: React.FC = () => {
-  return <View />;
+interface HeaderProps {
+  title: string;
+  action?: ReactNode;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, action }) => {
+  const { secondary100, secondary40, heading } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary100, secondary40]}
+    >
+      <BorderlessButton>
+        <Feather name="arrow-left" size={24} color={heading} />
+      </BorderlessButton>
+
+      <Text style={styles.title}>{title}</Text>
+
+      {action && <View>{action}</View>}
+    </LinearGradient>
+  );
 };
 
-export default Header;
+export { Header };
--- nlw-06/react-native/version-1.0/src/screens/AppointmentDetail/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentDetail/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { Background } from '../../components/Background';
+import { Header } from '../../components/Header';
 
 import { styles } from './styles';
 
 const AppointmentDetail: React.FC = () => {
-  return <Background />;
+  return (
+    <Background>
+      <Header />
+    </Background>
+  );
 };
 
-export default AppointmentDetail;
+export { AppointmentDetail };
