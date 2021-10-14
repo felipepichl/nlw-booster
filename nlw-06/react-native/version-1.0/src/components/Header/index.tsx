c98bb42c9a9457588ecffb2b8d29c6dedaa4c919
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-13 22:44:23 -0300

    NLW-06-ReactNative: Day-3: Header refactoring

--- nlw-06/react-native/version-1.0/src/components/Header/index.tsx
+++ nlw-06/react-native/version-1.0/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import { Text, View } from 'react-native';
 
@@ -15,13 +16,18 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, action }) => {
   const { secondary100, secondary40, heading } = theme.colors;
+  const { goBack } = useNavigation();
+
+  function handleGoBack() {
+    goBack();
+  }
 
   return (
     <LinearGradient
       style={styles.container}
       colors={[secondary100, secondary40]}
     >
-      <BorderlessButton>
+      <BorderlessButton onPress={handleGoBack}>
         <Feather name="arrow-left" size={24} color={heading} />
       </BorderlessButton>
 
