b243c763d4be0015503565cbcbac1414787c3c83
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-13 22:54:52 -0300

    NLW-06-ReactNative: Day-3: 36:47

--- nlw-06/react-native/version-1.0/src/screens/AppointmentDetails/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentDetails/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Fontisto } from '@expo/vector-icons';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 
 import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
 const AppointmentDetails: React.FC = () => {
   return (
     <Background>
-      <Header title="Detalhes" />
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto name="shared" size={24} color={theme.colors.primary} />
+          </BorderlessButton>
+        }
+      />
     </Background>
   );
 };
