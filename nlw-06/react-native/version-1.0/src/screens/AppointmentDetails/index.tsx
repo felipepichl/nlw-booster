f0a035715d8168f4cf1ad76115a98c3194a313dd
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-14 23:18:51 -0300

    NLW-06-ReactNative: Day-3: Doing list players

--- nlw-06/react-native/version-1.0/src/screens/AppointmentDetails/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentDetails/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Fontisto } from '@expo/vector-icons';
+import { ImageBackground, Text, View } from 'react-native';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
+import { ListHeader } from '../../components/ListHeader';
+
+import BannerImg from '../../assets/banner.png';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
@@ -19,6 +23,17 @@ const AppointmentDetails: React.FC = () => {
           </BorderlessButton>
         }
       />
+
+      <ImageBackground source={BannerImg} style={styles.banner}>
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>Lendários</Text>
+          <Text style={styles.subTitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida de md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader title="Jogadores" subTitle="Total 3" />
     </Background>
   );
 };
