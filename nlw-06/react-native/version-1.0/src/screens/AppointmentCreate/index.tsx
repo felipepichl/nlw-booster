0852ceeab5fcf8d75e53151edfc82193d811d956
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-17 23:15:52 -0300

    NLW-06-ReactNative: Day-3: 1:05:16

--- nlw-06/react-native/version-1.0/src/screens/AppointmentCreate/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentCreate/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './styles';
 
 const AppointmentCreate: React.FC = () => {
+  const [category, setCategory] = useState('');
+
   return (
     <Background>
       <Header title="Agendar Partida" />
 
       <Text style={styles.label}>Categoria</Text>
+
+      <CategorySelect
+        hasChackBox
+        categorySelected={category}
+        setCategory={setCategory}
+      />
+
+      <View style={styles.form}>
+        <RectButton>
+          <View style={styles.select} />
+        </RectButton>
+      </View>
     </Background>
   );
 };
