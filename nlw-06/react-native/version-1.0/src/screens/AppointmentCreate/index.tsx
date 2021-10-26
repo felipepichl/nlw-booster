c3d1e5a6d4f553e1e67254f20900655eee47bcd7
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-26 20:42:35 -0300

    NLW-06-ReactNative: Done AppointmentCreate

--- nlw-06/react-native/version-1.0/src/screens/AppointmentCreate/index.tsx
+++ nlw-06/react-native/version-1.0/src/screens/AppointmentCreate/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Feather } from '@expo/vector-icons';
 import { Text, View } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
@@ -7,6 +8,7 @@ import { Header } from '../../components/Header';
 import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
 const AppointmentCreate: React.FC = () => {
   const [category, setCategory] = useState('');
@@ -25,7 +27,19 @@ const AppointmentCreate: React.FC = () => {
 
       <View style={styles.form}>
         <RectButton>
-          <View style={styles.select} />
+          <View style={styles.select}>
+            <View style={styles.image} />
+
+            <View style={styles.selectBody}>
+              <Text style={styles.label}>Selecione um Servidor</Text>
+            </View>
+
+            <Feather
+              name="chevron-right"
+              color={theme.colors.heading}
+              size={18}
+            />
+          </View>
         </RectButton>
       </View>
     </Background>
