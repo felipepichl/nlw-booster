70306d0b368bfc4473406ed3c5a0151f77b7f96f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-10-28 23:08:57 -0300

    NLW-07-Node: Day-1: Refactoring routes

--- nlw-07/node-js/version-1.0/src/app/routes/authenticateUser.routes.ts
+++ nlw-07/node-js/version-1.0/src/app/routes/authenticateUser.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import { AuthenticateUserController } from '../controllers/AuthenticateUserController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const authenticateUserRouter = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+
+authenticateUserRouter.use(ensureAuthenticated);
 
 authenticateUserRouter.post('', authenticateUserController.handle);
 
--- nlw-07/node-js/version-1.0/src/config/auth.ts
+++ nlw-07/node-js/version-1.0/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'db262b92aeaae0774cf15ff17e18ee36',
+    secret: process.env.JWT_SECRET,
     expiresIn: '1d',
   },
 };
