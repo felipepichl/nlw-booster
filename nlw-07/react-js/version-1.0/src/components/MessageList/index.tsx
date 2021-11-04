11d0d816c2b1c3c8f7e2e3b1537d2e155fea607e
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-03 23:44:15 -0300

    NLW-07-React-JS: Day-2: Doing MessageList

--- nlw-07/react-js/version-1.0/src/components/MessageList/index.tsx
+++ nlw-07/react-js/version-1.0/src/components/MessageList/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg'
 
 const MessageList: React.FC = () => {
-  return <h1>MessageLIst</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageCOntent}>A new message</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/felipepichl.png" alt="Felipe Pichl" />
+            </div>
+            <span>Felipe Pichl</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export { MessageList };
