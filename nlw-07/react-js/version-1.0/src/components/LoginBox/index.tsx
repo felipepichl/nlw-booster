786ac2d8c0879f8857871f6c0636169ac603dc8a
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-10 23:30:24 -0300

    NLW-07-React-JS: Day-2: Fix LoginBox

--- nlw-07/react-js/version-1.0/src/components/LoginBox/index.tsx
+++ nlw-07/react-js/version-1.0/src/components/LoginBox/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { VscGithubInverted } from 'react-icons/vsc'
 
 import styles from './styles.module.scss'
 
 const LoginBox: React.FC = () => {
-  const signInUrl = `https://githunb.com/login/oauth/authorize?scope=user&clientId=${}$redirect_uri=http://localhost:3000`
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=9acd88c69d0f2e120d4e`
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithouCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithouCode);
+      
+    }
+  }, [])
 
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
 
-      <a href="#" className={styles.signInWtihGithub}>
+      <a href={signInUrl} className={styles.signInWtihGithub}>
         <VscGithubInverted size="24"/>
         Entrar com Github
       </a>
