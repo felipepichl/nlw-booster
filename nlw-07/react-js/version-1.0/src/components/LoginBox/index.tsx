aa0e2de3a2fd4324cab3b79d6e6e71bdb22d9eef
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-10 23:48:54 -0300

    NLW-07-React-JS: Day-2: 1:14:17

--- nlw-07/react-js/version-1.0/src/components/LoginBox/index.tsx
+++ nlw-07/react-js/version-1.0/src/components/LoginBox/index.tsx
@@ -1,11 +1,32 @@
 import React, { useEffect } from 'react';
 
 import { VscGithubInverted } from 'react-icons/vsc'
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss'
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    avatar_url: string;
+    login: string;
+  }
+}
+
 const LoginBox: React.FC = () => {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=9acd88c69d0f2e120d4e`
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token)
+  }
 
   useEffect(() => {
     const url = window.location.href;
