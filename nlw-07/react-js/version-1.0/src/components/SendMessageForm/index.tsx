928aa2db73e84bcee45c3de4727dff7537b1738c
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-16 20:21:10 -0300

    NLW-07-React-JS: Day-2: Doing SendMessageForm tsx

--- nlw-07/react-js/version-1.0/src/components/SendMessageForm/index.tsx
+++ nlw-07/react-js/version-1.0/src/components/SendMessageForm/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
 
 import styles from './styles.module.scss';
 
 const SendMessageForm: React.FC = () => {
-  return <div />;
+  const { user } = useContext(AuthContext)
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton}>
+        <VscSignOut size="32"/>
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size="16"/>
+          {user?.login}
+        </span>
+      </header>
+    </div>
+  );
 }
 
 export { SendMessageForm };
