6d4cd7d8e16b657defa569d8e04071c90c898203
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-18 22:37:37 -0300

    NLW-07-React-JS: Day-2: Doing handleSendMessage

--- nlw-07/react-js/version-1.0/src/components/SendMessageForm/index.tsx
+++ nlw-07/react-js/version-1.0/src/components/SendMessageForm/index.tsx
@@ -1,12 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, FocusEvent, FormEvent } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
 
 import styles from './styles.module.scss';
+import { api } from '../../services/api';
 
 const SendMessageForm: React.FC = () => {
   const { user, signOut } = useContext(AuthContext)
+  const [message, setMessage] = useState('')
+
+  async function handleSendMessage(event: FormEvent){
+    event.preventDefault()
+    
+    if(!message.trim()) {
+      return;
+    }
+
+    await api.post('message', {
+      message
+    })
+  }
 
   return (
     <div className={styles.sendMessageFormWrapper}>
@@ -26,12 +40,14 @@ const SendMessageForm: React.FC = () => {
         </span>
       </header>
 
-      <form className={styles.sendMessageForm}>
+      <form onSubmit={handleSendMessage } className={styles.sendMessageForm}>
         <label htmlFor="message">Mensagem</label>
         <textarea 
           name="message"
           id="message"
           placeholder="Qual sua expectativa para o evento?" 
+          onChange={event => setMessage(event.target.value)}
+          value={message}
         />
 
         <button type="submit">Enviar mensagem</button>
