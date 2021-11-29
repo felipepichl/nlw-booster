4572db3df524bca7fcca047837d08a632a9cc81c
Felipe Pichl <felipe.pichl@hotmail.com>
2021-11-28 22:21:22 -0300

    NLW-07-React-Native: Day-3: Doing MessageList

--- nlw-07/react-native/version-1.0/src/components/Message/index.tsx
+++ nlw-07/react-native/version-1.0/src/components/Message/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+
+import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
 
 const Message: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.message}>Texto da mensagem</Text>
+
+      <View>
+        <UserPhoto
+          imageUri="https://github.com/felipepichl.png"
+          sizes="SMALL"
+        />
+
+        <Text style={styles.userName}>Felipe Pichl</Text>
+      </View>
+    </View>
+  );
 };
 
 export { Message };
