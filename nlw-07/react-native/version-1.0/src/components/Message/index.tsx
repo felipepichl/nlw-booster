1261e9f492f647b53fab2745321ae86c5a1883cd
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-01 22:14:26 -0300

    NLW-07-React-Native: Day-3: Animation with moti

--- nlw-07/react-native/version-1.0/src/components/Message/index.tsx
+++ nlw-07/react-native/version-1.0/src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 
 import { UserPhoto } from '../UserPhoto';
 
@@ -20,7 +21,12 @@ type Props = {
 
 const Message: React.FC<Props> = ({ data }) => {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
 
       <View style={styles.footer}>
@@ -28,7 +34,7 @@ const Message: React.FC<Props> = ({ data }) => {
 
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 };
 
