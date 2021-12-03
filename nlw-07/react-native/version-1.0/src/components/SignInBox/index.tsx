70e4a25aa713938ab8f0e83a66fc0e8c4ee2934b
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-03 19:28:18 -0300

    NLW-07-React-Native: Day-3: Doing SignInBox

---
+++ nlw-07/react-native/version-1.0/src/components/SignInBox/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+const SignInBox: React.FC = () => {
+  return <View style={styles.container} />;
+};
+
+export { SignInBox };
