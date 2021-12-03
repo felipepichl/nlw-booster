4f72dc157de9a6e54f97e986216df46919dc1b96
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-02 23:02:58 -0300

    NLW-07-React-Native: Day-3: Done Button css

--- nlw-07/react-native/version-1.0/src/components/Button/styles.ts
+++ nlw-07/react-native/version-1.0/src/components/Button/styles.ts
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
+import { FONTS } from '../../theme';
 
 const styles = StyleSheet.create({
-  container: {},
+  button: {
+    width: 48,
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: FONTS.BOLD,
+  },
+  icon: {
+    marginRight: 12,
+  },
 });
 
 export { styles };
