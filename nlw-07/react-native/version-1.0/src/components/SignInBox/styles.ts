b63377188795426f87e49bbbaebd951124882378
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-03 19:33:02 -0300

    NLW-07-React-Native: Day-3: Done SignInBox css

--- nlw-07/react-native/version-1.0/src/components/SignInBox/styles.ts
+++ nlw-07/react-native/version-1.0/src/components/SignInBox/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: getBottomSpace() + 32,
+  },
 });
 
 export { styles };
