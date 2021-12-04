ffdb75a04eb76e09e8841d532bbf71fb535c93dd
Felipe Pichl <felipe.pichl@hotmail.com>
2021-12-04 00:03:42 -0300

    NLW-07-React-Native: Day-3: Done Button

--- nlw-07/react-native/version-1.0/src/components/Button/index.tsx
+++ nlw-07/react-native/version-1.0/src/components/Button/index.tsx
@@ -4,7 +4,9 @@ import {
   TouchableOpacityProps,
   Text,
   ColorValue,
+  ActivityIndicator,
 } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 import { styles } from './styles';
 
@@ -12,17 +14,33 @@ type Props = TouchableOpacityProps & {
   title: string;
   color: ColorValue;
   backgroundColor: ColorValue;
+  icon?: React.ComponentProps<typeof AntDesign>['name'];
+  isLoading?: boolean;
 };
 
 const Button: React.FC<Props> = ({
   title,
   color,
   backgroundColor,
+  icon,
+  isLoading = false,
   ...rest
 }) => {
   return (
-    <TouchableOpacity style={[styles.button, { backgroundColor }]} {...rest}>
-      <Text style={[styles.title, { color }]}>{title}</Text>
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor }]}
+      activeOpacity={0.7}
+      disabled={isLoading}
+      {...rest}
+    >
+      {isLoading ? (
+        <ActivityIndicator color={color} />
+      ) : (
+        <>
+          <AntDesign name={icon} size={24} style={styles.icon} />
+          <Text style={[styles.title, { color }]}>{title}</Text>
+        </>
+      )}
     </TouchableOpacity>
   );
 };
--- nlw-07/react-native/version-1.0/src/components/SignInBox/index.tsx
+++ nlw-07/react-native/version-1.0/src/components/SignInBox/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
+import { COLORS } from '../../theme';
+
+import { Button } from '../Button';
 
 import { styles } from './styles';
 
 const SignInBox: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <Button
+        title="ENTRAR COM GITHUB"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"
+      />
+    </View>
+  );
 };
 
 export { SignInBox };
