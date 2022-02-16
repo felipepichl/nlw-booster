7ac4dfeee6fe4687bab157940a80ec749aed5480
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-15 23:13:20 -0300

    NLW-07-React-Native: Day-3: Done day 3

--- nlw-07/react-native/version-1.0/src/components/MessageList/index.tsx
+++ nlw-07/react-native/version-1.0/src/components/MessageList/index.tsx
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
+import { io } from 'socket.io-client';
 import { api } from '../../services/api';
 
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+
+const messageQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', newMessage => {
+  messageQueue.push(newMessage);
+});
 
 const MessageList: React.FC = () => {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
@@ -17,6 +25,21 @@ const MessageList: React.FC = () => {
     }
 
     fetchMssages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messageQueue.length > 0) {
+        setCurrentMessages(prevState => [
+          messageQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messageQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
   }, []);
 
   return (
--- nlw-07/react-native/version-1.0/src/screens/Home/index.tsx
+++ nlw-07/react-native/version-1.0/src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import { useAuth } from '../../hooks/auth';
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
 import { SignInBox } from '../../components/SignInBox';
 import { SendMessageForm } from '../../components/SendMessageForm';
+
+import { useAuth } from '../../hooks/auth';
 
 import { styles } from './styles';
 
@@ -13,12 +14,17 @@ const Home: React.FC = () => {
   const { user } = useAuth();
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <MessageList />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
 
-      {user ? <SendMessageForm /> : <SignInBox />}
-    </View>
+        {user ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
