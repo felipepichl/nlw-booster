fbb9895d4964f09afdfad24efccdf7978d1ba998
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-26 15:45:25 -0300

    NLW-06-ReactJS: Day-1: .ENV

--- nlw-06/react-js/version-1.0/src/services/firebase.ts
+++ nlw-06/react-js/version-1.0/src/services/firebase.ts
@@ -1,19 +1,19 @@
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
-import firebaseAuth from 'firebase/auth';
-import firevaseDatabase from 'firebase/database';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDq3bO7rREhekiENJ5bRocqMOyc4eDn37M',
-  authDomain: 'letmeask-nlw06-3c5e1.firebaseapp.com',
-  databaseURL: 'https://letmeask-nlw06-3c5e1-default-rtdb.firebaseio.com',
-  projectId: 'letmeask-nlw06-3c5e1',
-  storageBucket: 'letmeask-nlw06-3c5e1.appspot.com',
-  messagingSenderId: '352754297846',
-  appId: '1:352754297846:web:25c3abe838a9da776d35d7',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = firebaseAuth.getAuth();
-export const database = firevaseDatabase.getDatabase();
+export const auth = firebase.auth();
+export const database = firebase.database();
