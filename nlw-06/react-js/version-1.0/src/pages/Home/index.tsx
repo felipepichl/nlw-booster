57772a2d1c25b9e27571131c91d065250f1b0697
Felipe Pichl <felipe.pichl@hotmail.com>
2022-02-26 17:17:13 -0300

    NLW-06-ReactJS: Day-1: Doing Home

--- nlw-06/react-js/version-1.0/src/pages/Home/index.tsx
+++ nlw-06/react-js/version-1.0/src/pages/Home/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-import { Container } from './styles';
+// import { Container } from './styles';
+import illustration from '../../assets/illustration.svg';
+import logo from '../../assets/logo.svg';
+import googleIconImg from '../../assets/google-icon.svg';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <h1>Hello World - Home</h1>
-    </Container>
+    <div>
+      <aside>
+        <img src={illustration} alt="Illustration" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sa audiência em tempo real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logo} alt="letmeask" />
+          <button>
+            <img src={googleIconImg} alt="Google" />
+            Crie sua sala com o Google
+          </button>
+        </div>
+      </main>
+    </div>
   );
 };
 
