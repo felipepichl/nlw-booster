38184e573b8d0db3eaed6df1b6d6a9d61d47a9d1
Felipe Pichl <felipe.pichl@hotmail.com>
2022-03-24 13:34:15 -0300

    NLW-06-ReactJS: Day-3: Done create for rooms

--- nlw-06/react-js/version-1.0/src/pages/NewRoom/index.tsx
+++ nlw-06/react-js/version-1.0/src/pages/NewRoom/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-// import { AuthContext } from '../../contexts/AuthContext';
+import { database } from '../../services/firebase';
+import { useAuth } from '../../hooks/useAuth';
 
 import { Button } from '../../components/Button';
 import { Aside } from '../../components/Aside';
@@ -11,7 +12,23 @@ import { Container } from './styles';
 import logo from '../../assets/logo.svg';
 
 const NewRoom: React.FC = () => {
-  // const { user } = useContext(AuthContext);
+  const [newRoom, setNewRoom] = useState('');
+  const { user } = useAuth();
+
+  async function handleCreateNewRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+  }
 
   return (
     <Container>
@@ -20,8 +37,13 @@ const NewRoom: React.FC = () => {
         <div>
           <img src={logo} alt="letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateNewRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
 
             <Button title="Criar sala" />
           </form>
