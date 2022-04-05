813152b0f5c8efccd7c6a7d1e51098949468e7c8
Felipe Pichl <felipe.pichl@hotmail.com>
2022-04-05 13:32:14 -0300

    NLW-06-ReactJS: Day-3: Send a new question

--- nlw-06/react-js/version-1.0/src/pages/Room/index.tsx
+++ nlw-06/react-js/version-1.0/src/pages/Room/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
 
 import { Container, Content, RoomTitle, FormFooter } from './styles';
 
+type RoomParams = {
+  id: string;
+};
+
 const Room: React.FC = () => {
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const [newQuestion, setNewQuestion] = useState('');
+
+  async function handleNewQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+  }
+
   return (
     <Container>
       <header>
         <Content>
           <img src={logoImg} alt="Logo" />
-          <RoomCode code="-Mz0vw-kYAvSoskHu6oN" />
+          <RoomCode code={roomId} />
         </Content>
       </header>
 
@@ -23,15 +59,19 @@ const Room: React.FC = () => {
           <span>4 perguntas</span>
         </RoomTitle>
 
-        <form>
-          <textarea placeholder="What do you ask?" />
+        <form onSubmit={handleNewQuestion}>
+          <textarea
+            placeholder="What do you ask?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
 
           <FormFooter>
             <span>
               For send a question,
               <button type="button">SignIn</button>
             </span>
-            <Button type="submit" title="Send a question" />
+            <Button type="submit" title="Send a question" disabled={!user} />
           </FormFooter>
         </form>
       </main>
