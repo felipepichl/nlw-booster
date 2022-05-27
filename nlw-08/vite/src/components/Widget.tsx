aab80f82697e6a8c72bcf660f64d3e792b4aa9d0
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-26 21:55:58 -0300

    NLW-08-React-JS: Day-1: Toogle

--- nlw-08/vite/src/components/Widget.tsx
+++ nlw-08/vite/src/components/Widget.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChatTeardropDots } from 'phosphor-react'
 
 const Widget: React.FC = () => {
-  return (
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
 
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+  
+  return (
+    
     <div className='absolute bottom-5 right-5'>
-      <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
+      {isWidgetOpen && (<p>Hello Wolrd</p>)}
+      
+      
+      <button className='
+        bg-brand-500 
+        rounded-full 
+        px-3 h-12 
+        text-white 
+        flex 
+        items-center 
+        group'
+        onClick={toggleWidgetVisibility}
+      >
         <ChatTeardropDots className='w-6 h-6' />
 
-        <span>Feedback</span>
+        <span className='
+          max-w-0 
+          overflow-hidden 
+          group-hover:max-w-xs 
+          transition-all 
+          duration-500
+          ease-linear'
+        >
+          <span className='pl-2'></span>
+          Feedback
+        </span>
       </button>
     </div>
-
   );
 }
 
