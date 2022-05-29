ed4a65184ffe15c2524709a78ae4f47368dee648
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-28 23:05:41 -0300

    NLW-08-React-JS: Day-2: Doing WidgetForm

---
+++ nlw-08/vite/src/components/WidgetForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WidgetForm: React.FC = () => {
+  return (
+    <div className='
+      bg-zinc-900 
+      p-4 realtive 
+      rounded-2xl 
+      mb-4 flex 
+      flex-col'
+    />
+  )
+}
+
+export default WidgetForm;
