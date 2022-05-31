bb0cddcc1aacbe20fbdc940e54d0f61edf835f64
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-30 22:24:51 -0300

    NLW-08-React-JS: Day-2: 22:19

--- nlw-08/vite/src/components/WidgetForm.tsx
+++ nlw-08/vite/src/components/WidgetForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CloseButton from './CloseButton';
 
 const WidgetForm: React.FC = () => {
   return (
@@ -11,7 +12,19 @@ const WidgetForm: React.FC = () => {
       shadow-lg
       w-[calc(100vw-2rem)]
       md:w-auto'
-    />
+    >
+      <header>
+        <span className='text-xl leading-6'>Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <p>Hello World</p>
+
+      <footer className='text-xs text-neutral-400'>
+        Feito com 💜 pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+      </footer>
+    </div>
   )
 }
 
