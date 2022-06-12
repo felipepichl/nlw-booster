8ad8898df9821defba8cb302e5f625bc73b8fab8
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-11 22:13:15 -0300

    NLW-08-React-JS: Day-2: Done button feedback

--- nlw-08/vite/src/components/WidgetForm/Stepes/FeedbackContentStep.tsx
+++ nlw-08/vite/src/components/WidgetForm/Stepes/FeedbackContentStep.tsx
@@ -61,6 +61,32 @@ const FeedbackContentStep: React.FC<FeedbackContentStepProps> = ({
           '
           placeholder='Tell me with more details What`s happen...'
         />
+
+        <footer className='flex gap-2 mt-2'>
+          <button
+            type='submit'
+            className='
+              p-2
+              bg-brand-500
+              rounded-md
+              border-transparent
+              flex-1
+              flex
+              justify-center
+              items-center
+              text-sm
+              hover:bg-brand-300
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-offset-zinc-900
+              focus:ring-brand-500 
+              transition-colors
+            '
+          >
+            Enviar feedback
+          </button>
+        </footer>
       </form>
     </>
     );
