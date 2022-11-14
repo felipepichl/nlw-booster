ece7f9fd9bc0204e9d3ddfc9755044c2452dcc28
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-14 18:01:54 -0300

    NLW-01-Server-2.0 - Item providers - Doing

--- nlw-01/version-2.0/server/src/shared/container/index.ts
+++ nlw-01/version-2.0/server/src/shared/container/index.ts
@@ -3,10 +3,10 @@ import { container } from 'tsyringe';
 import './providers';
 
 import { IItemsRepository } from '@modules/items/repositories/IItemsRepository';
-import { ItemsRepository } from '@modules/items/infra/typeorm/repositories/ItemsRepository';
+import { ItemsRepository } from '@modules/items/infra/prisma/repositories/ItemsRepository';
 
-import { IPointsRepository } from '@modules/Points/repositories/IPointsRepository';
-import { PointsRepository } from '@modules/Points/infra/typeorm/repositories/PointsRepository';
+import { IPointsRepository } from '@modules/points/repositories/IPointsRepository';
+import { PointsRepository } from '@modules/points/infra/typeorm/repositories/PointsRepository';
 
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
