a09e03e9be4315e3df77581c3f0cb197de6ea1f6
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-17 18:25:35 -0300

    NLW-01-Server-2.0 - Point repository doing

--- nlw-01/version-2.0/server/src/modules/points/infra/prisma/repositories/PointsRepository.ts
+++ nlw-01/version-2.0/server/src/modules/points/infra/prisma/repositories/PointsRepository.ts
@@ -1,10 +1,42 @@
-import { ICreatePointDTO } from '@modules/points/dtos/ICreatePointDTO';
-import { IPointsRepository } from '@modules/points/repositories/IPointsRepository';
+import { PrismaClient } from '@prisma/client';
+
+import { ICreatePointDTO } from '../../../dtos/ICreatePointDTO';
+import { IPointsRepository } from '../../../repositories/IPointsRepository';
 import { Point } from '../models/Point';
 
 class PointsRepository implements IPointsRepository {
-  create(data: ICreatePointDTO): Promise<Point> {
-    throw new Error('Method not implemented.');
+  prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async create({
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+    image,
+    user_id,
+  }: ICreatePointDTO): Promise<Point> {
+    const result = await this.prisma.point.create({
+      data: {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        point_items: items,
+        image,
+        fk_user_id: user_id,
+      },
+    });
   }
 
   findOne(id: string): Promise<Point> {
