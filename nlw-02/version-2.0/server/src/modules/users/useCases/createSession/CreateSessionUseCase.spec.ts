d31f3d66b8dc8eb8ebb5c8d3c974c43f73bd1019
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-28 18:05:53 -0300

    NLW-02-Server-2.0 - Done Session - Tests

--- nlw-02/version-2.0/server/src/modules/users/useCases/createSession/CreateSessionUseCase.spec.ts
+++ nlw-02/version-2.0/server/src/modules/users/useCases/createSession/CreateSessionUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '@shared/errors/AppError';
 import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
 import { HashProviderInMemory } from '../../provider/HashProvider/in-memory/HashProviderInMemory';
 
@@ -36,5 +37,43 @@ describe('Create Session', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to create a new session with wrong email', async () => {
+    expect(async () => {
+      await usersRepositoryInMemory.create({
+        name: 'User Test',
+        username: 'user_test',
+        email: 'test@test.com',
+        password: 'hash123',
+        avatar: 'https://example.com/user_test.png',
+        bio: 'A great bio',
+        whatsapp: '55999998888',
+      });
+
+      await createSessionUseCase.execute({
+        email: 'worn_email@wrong.com',
+        password: 'hash123',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new session with wrong password', async () => {
+    expect(async () => {
+      await usersRepositoryInMemory.create({
+        name: 'User Test',
+        username: 'user_test',
+        email: 'test@test.com',
+        password: 'hash123',
+        avatar: 'https://example.com/user_test.png',
+        bio: 'A great bio',
+        whatsapp: '55999998888',
+      });
+
+      await createSessionUseCase.execute({
+        email: 'test@test.com',
+        password: 'wrong_password',
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
