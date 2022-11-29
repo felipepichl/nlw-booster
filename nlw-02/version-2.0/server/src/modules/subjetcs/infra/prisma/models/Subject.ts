8d657fc4cdaa4dabd328a75dd59bda9191e1e0a3
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-29 15:58:39 -0300

    NLW-02-Server-2.0 - Subject, In-Memory

--- nlw-02/version-2.0/server/src/modules/subjetcs/infra/prisma/models/Subject.ts
+++ nlw-02/version-2.0/server/src/modules/subjetcs/infra/prisma/models/Subject.ts
@@ -1,3 +1,20 @@
-class Subject {}
+import { v4 as uuid } from 'uuid';
+import { Subject as ISubject } from '@prisma/client';
+
+class Subject implements ISubject {
+  id: string;
+
+  titile: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
 
 export { Subject };
---
+++ nlw-02/version-2.0/server/src/modules/subjetcs/repositories/in-memory/SubjectsRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { Subject } from '../../infra/prisma/models/Subject';
+import { ICreateSubjectDTO } from '../../dtos/ICreateSubjectDTO';
+import { ISubjetcsRepository } from '../ISubjetcsRepository';
+
+class SubjectsRepositoryInMemory implements ISubjetcsRepository {
+  subjects: Subject[] = [];
+
+  async create({ title }: ICreateSubjectDTO): Promise<Subject> {
+    const subject = new Subject();
+
+    Object.assign(subject, title);
+
+    this.subjects.push(subject);
+
+    return subject;
+  }
+
+  async list(): Promise<Subject[]> {
+    return this.subjects;
+  }
+}
+
+export { SubjectsRepositoryInMemory };
