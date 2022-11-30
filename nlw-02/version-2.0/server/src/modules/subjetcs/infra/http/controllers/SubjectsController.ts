1d2b1b8211ed06185bf96c110cdc223ab0441b19
Felipe Pichl <felipe.pichl@hotmail.com>
2022-11-30 11:19:02 -0300

    NLW-02-Server-2.0 - Subject, https

---
+++ nlw-02/version-2.0/server/src/modules/subjetcs/infra/http/controllers/SubjectsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateSubjectUseCase } from '../../../useCases/createSubjects/CreateSubjectUseCase';
+
+class SubjectsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { title } = request.body;
+
+    const createSubjectUseCase = container.resolve(CreateSubjectUseCase);
+
+    const subject = await createSubjectUseCase.execute({
+      title,
+    });
+
+    return response.json(subject);
+  }
+}
+
+export { SubjectsController };
---
+++ nlw-02/version-2.0/server/src/modules/subjetcs/infra/http/routes/subject.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+
+import { SubjectsController } from '../controllers/SubjectsController';
+
+const subjectsRouter = Router();
+
+const subjectsController = new SubjectsController();
+// const listSubjectsController = new ListSubjectsController();
+
+subjectsRouter.use(ensureAuthenticated);
+
+subjectsRouter.post('', subjectsController.handle);
+// subjectsRouter.get('', listSubjectsController.handle);
+
+export { subjectsRouter };
