8e94357e911145f5c7a51803569163f34a1f8697
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-01 11:48:31 -0300

    NLW-02-Server-2.0 - Config app and server

---
+++ nlw-02/version-2.0/server/src/modules/users/infra/http/controllers/CreateUsersController.e2e.spec.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+//import {} from '@shared/infra/http/start/server'
+
+describe('E2E Users', () => {
+
+
+  it('should be able to create a new user', () => {
+
+  })
+})
--- nlw-02/version-2.0/server/src/shared/infra/http/start/server.ts
+++ nlw-02/version-2.0/server/src/shared/infra/http/start/server.ts
@@ -1,29 +1,4 @@
-import 'reflect-metadata';
-
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-// import '../database';
-
-import { AppError } from 'app/error/AppError';
-
-import routes from '../routes';
-import '@shared/container';
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-app.use(
-  (err: Error, request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      message: `Internal server error ${err.message}`,
-    });
-  },
-);
+import { app } from './app';
 
 app.listen(3333, () => {
   console.log('Server running in port 3333');
