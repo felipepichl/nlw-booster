b5fd4b800a10f25a089ac6c01c6c1a0f80ead7ae
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-06 11:27:07 -0300

    NLW-02-Server-2.0 - Done E2E Sessions

--- nlw-02/version-2.0/server/src/modules/users/infra/http/controllers/createSession/CreateSessionsController.e2e-spec.ts
+++ nlw-02/version-2.0/server/src/modules/users/infra/http/controllers/createSession/CreateSessionsController.e2e-spec.ts
@@ -1,25 +1,27 @@
+import { hash } from 'bcryptjs';
 import request from 'supertest';
+
 import { app } from '@shared/infra/http/start/app';
+
 
 import { prisma } from '@shared/infra/prisma'
 
 describe('E2E Sessions', () => {
   beforeEach(async () => {
 
+    const passwordHash = await hash('hash123', 8)
+
     const result = await prisma.user.create({
       data: {
         name: 'Teste',
         username: 'felipepichl',
         email: 'test@teste.com',
-        password: 'hash123',
+        password: passwordHash,
         avatar: 'https://example.com/user_test.png',
         bio: 'A great user test',
         whatsapp: '55999998888',
       }
     });
-
-    console.log(result);
-
   })
 
   it('should be able to create a new session', async () => {
