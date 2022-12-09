e4a9f803abb5faae7297707c4ccdb858bcad364b
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-09 11:21:43 -0300

    NLW-02-Server-2.0 - Test for create classes

---
+++ nlw-02/version-2.0/server/src/modules/classes/useCases/createClasses/CreateClassesUseCase.spec.ts
@@ -0,0 +1,23 @@
+import { ClassesRepositoryInMemory } from "../../repositories/in-memory/ClassesRepositoryInMemory";
+import { CreateClassesUseCase } from "./CreateClassesUseCase";
+
+let classesRepositoryInMemory: ClassesRepositoryInMemory;
+let createClassesUseCase: CreateClassesUseCase;
+
+describe("Create Classes", () => {
+  beforeEach(() => {
+    classesRepositoryInMemory = new ClassesRepositoryInMemory();
+
+    createClassesUseCase = new CreateClassesUseCase(classesRepositoryInMemory);
+  });
+
+  it("shoul be abre to create a new class", async () => {
+    const classes = await createClassesUseCase.execute({
+      cost: 100,
+      subject_id: "subject_id",
+      user_id: "user_id",
+    });
+
+    expect(classes).toHaveProperty("id");
+  });
+});
