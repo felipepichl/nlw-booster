5f4cdc6de9fd83dc700a81dadfc3afd16db030d1
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-19 16:38:09 -0300

    NLW-02-Server-2.0 - ListAllClasses e2e test II

--- nlw-02/version-2.0/server/src/modules/classes/infra/http/controllers/listAllClasses/ListAllClassesController.e2e-spec.ts
+++ nlw-02/version-2.0/server/src/modules/classes/infra/http/controllers/listAllClasses/ListAllClassesController.e2e-spec.ts
@@ -1,3 +1,14 @@
-import {} from "@shared/infra/http/start/app";
+import request from "supertest";
 
-describe("[E2E] - List all Classes", () => {});
+import { app } from "@shared/infra/http/start/app";
+
+describe("[E2E] - List all Classes", () => {
+  it("should be able to list all classes", async () => {
+    const responseToken = await request(app).post("/sessions");
+
+    const response = await request(app).get("/classes");
+
+    expect(response.body.error).toBeFalsy();
+    expect(response.status).toBe(200);
+  });
+});
