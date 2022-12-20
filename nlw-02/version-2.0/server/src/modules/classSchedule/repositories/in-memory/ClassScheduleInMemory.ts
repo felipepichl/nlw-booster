11f27c9ddbd6ccdef18595ffe7fb8c74dd305647
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-20 17:06:59 -0300

    NLW-02-Server-2.0 - Doing ClassSchedule in-memory

---
+++ nlw-02/version-2.0/server/src/modules/classSchedule/repositories/in-memory/ClassScheduleInMemory.ts
@@ -0,0 +1,34 @@
+import { ICreateClassSchedule } from "@modules/classSchedule/dtos/ICreateClassSchedule";
+import { ClassSchedule } from "@modules/classSchedule/infra/prisma/ClassSchedule";
+
+import { IClassScheduleRepository } from "../IClassScheduleRepository";
+
+class ClassScheduleInMemory implements IClassScheduleRepository {
+  private classSchedules: ClassSchedule[] = [];
+
+  async create(data: ICreateClassSchedule): Promise<ClassSchedule> {
+    const classSchedule = new ClassSchedule();
+
+    Object.assign(classSchedule, data);
+
+    this.classSchedules.push(classSchedule);
+
+    return classSchedule;
+  }
+  async list(
+    week_day: string,
+    subject: string,
+    time: string
+  ): Promise<ClassSchedule[]> {
+    /**
+     * List all classes with sibject_id and with map return all ids in a array
+     * in result insert this array Ex: fk_class_id = IN(allClassesIds)
+     */
+
+    return this.classSchedules.filter(
+      (classSchedule) => classSchedule.week_day === Number(week_day)
+    );
+  }
+}
+
+export { ClassScheduleInMemory };
