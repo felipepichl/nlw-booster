d5277e01a105908c6762b1b02e7f8db695a97bd8
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-28 17:37:18 -0300

    NLW-02-Server-2.0 - UniqueEntityID, Entity

--- nlw-02/version-2.0/server/src/shared/domain/UniqueEntityID.ts
+++ nlw-02/version-2.0/server/src/shared/domain/UniqueEntityID.ts
@@ -6,6 +6,26 @@ class UniqueEntityID {
   constructor(id?: string) {
     this.value = id || uuid();
   }
+
+  public equals(id?: UniqueEntityID): boolean {
+    if (id === null || id === undefined) {
+      return false;
+    }
+
+    if (!(id instanceof UniqueEntityID)) {
+      return false;
+    }
+
+    return id.toValue() === this.value;
+  }
+
+  toString(): string {
+    return String(this.value);
+  }
+
+  toValue(): string {
+    return this.value;
+  }
 }
 
 export { UniqueEntityID };
