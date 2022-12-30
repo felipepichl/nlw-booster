fc97b7ee9cec4171059c3a3e1d413b18dce6c6ca
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-30 11:40:25 -0300

    NLW-02-Server-2.0 - Remove prisma models

--- nlw-02/version-2.0/server/src/modules/users/infra/prisma/repositories/UsersRepository.ts
+++ nlw-02/version-2.0/server/src/modules/users/infra/prisma/repositories/UsersRepository.ts
@@ -1,12 +1,11 @@
+import { User } from "@modules/users/domain/User";
 import { ICreateUserDTO } from "@modules/users/dtos/ICreateUserDTO";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
 
 import { prisma } from "@shared/infra/prisma";
 
-import { User } from "../models/User";
-
 class UsersRepository implements IUsersRepository {
-  async create(data: ICreateUserDTO): Promise<User> {
+  async create(data: ICreateUserDTO): Promise<void> {
     const result = await prisma.user.create({
       data,
     });
