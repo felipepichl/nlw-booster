882cd67d03b8eaf84b94e63cfb039b322442fb24
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-03 17:50:51 -0300

    NLW-02-Server-2.0 - Doing Result II

--- nlw-02/version-2.0/server/src/shared/core/logic/Result.ts
+++ nlw-02/version-2.0/server/src/shared/core/logic/Result.ts
@@ -1,3 +1,22 @@
-class Result {}
+class Result<T> {
+  public isSuccess: boolean;
+
+  public isFailure: boolean;
+
+  public error: T | string;
+
+  constructor(isSuccess: boolean, error?: T | string, value?: string) {
+    if (isSuccess && error) {
+      throw new Error("InvalidOperation");
+    }
+
+    if (!isSuccess && error) {
+      throw new Error("InvalidOperation");
+    }
+
+    this.isSuccess = isSuccess;
+    this.isFailure = !isSuccess;
+  }
+}
 
 export { Result };
